8a61922e697794fa59a643d9ada37ce6cc65b991
sujan <dev.mern.sujan@gmail.com>
2023-02-14 11:50:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const chalk = require("chalk");
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(8000, function () {
+  console.log(chalk.bgBlue("Port running on 8000 port"));
+});
